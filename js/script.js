a7f27f0dd41086ceebc5bfa01700501ac52ac9ab
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-03-11 09:51:55 +0200

    make hw

--- js/script.js
+++ js/script.js
@@ -8,18 +8,37 @@ list.addEventListener("keyup", (event) => {
 });
 
 //--2--
-const controls = document.querySelector("#controls");
-const input = document.querySelector("input");
-const btnRender = document.querySelector('[data-action="render"]');
-const btnDestroy = document.querySelector('[data-action="destroy"]');
-const boxes = document.querySelector("#boxes");
-input.addEventListener("input", createBoxes);
+const refs = {
+  controls: document.querySelector("#controls"),
+  input: document.querySelector("input"),
+  btnRender: document.querySelector('[data-action="render"]'),
+  btnDestroy: document.querySelector('[data-action="destroy"]'),
+  boxes: document.querySelector("#boxes"),
+};
+refs.btnRender.addEventListener("click", createBoxes);
+refs.btnDestroy.addEventListener("click", destoryBoxes);
 
 function createBoxes(amount) {
-  if (btnRender) {
-    const divEL = document.createElement("div");
-    const newDiv = boxes.append(divEL);
-    amount = input.value;
-    console.log(amount);
+  amount = 30;
+  const inputEl = refs.input.value;
+  for (let i = 0; i < inputEl; i++) {
+    const createDiv = document.createElement("div");
+
+    createDiv.style.width = amount + "px";
+    createDiv.style.height = amount + "px";
+    createDiv.style.backgroundColor = randomeColor();
+
+    amount += 10;
+
+    refs.boxes.append(createDiv);
   }
+}
+function destoryBoxes() {
+  refs.boxes.innerHTML = "";
+}
+function randomeColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
